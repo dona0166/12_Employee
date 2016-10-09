ac4ee0f3f7652655b41c5a0cfd2642066c691ce6
Donnie <Donnie@DESKTOP-A0TIJCR>
2016-10-09 09:24:14 +0200

    Add project files.

---
+++ 12_Employee/Employee.cs
@@ -0,0 +1,19 @@
+﻿namespace _12_Employee
+{
+    internal class Employee
+    {
+        public string _name;
+        public string _job;
+
+        public Employee()
+        {
+
+        }
+
+        public Employee(string name, string job)
+        {
+            this._name = name;
+            this._job = job;
+        }
+    }
+}
---
+++ 12_Employee/EmployeeRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace _12_Employee
+{
+    internal class EmployeeRepository 
+    {
+        Dictionary<string, Employee> adictionary = new Dictionary<string, Employee>();
+        
+        public EmployeeRepository()
+        {
+            
+        }
+        
+
+        public void Clear()
+        {
+            adictionary.Clear();
+            
+        }
+
+        public int CountEmployees()
+        {
+            return adictionary.Count;
+        }
+        Employee anemployee;
+        public Employee CreateEmployee(string name, string job)
+        {
+            anemployee = new Employee(name, job);
+            adictionary.Add(name, anemployee);
+            return anemployee;
+        }
+    }
+}
